43f27f7169285992b875981ef84c5eb836d1dc87
Allison MacQueen <allimacq@gmail.com>
2020-05-26 19:02:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 != 0
       puts "Fizz"
-    elsif num % 5 == 0
+    elsif num % 5 == 0 && num % 3 != 0
       puts "Buzz"
     elsif num % 15 == 0
       puts "FizzBuzz"
