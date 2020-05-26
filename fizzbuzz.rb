64797235993aa9a34622a010e20fbc2df283f865
Allison MacQueen <allimacq@gmail.com>
2020-05-26 18:37:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-  else
-    puts 'nil'
+  #elsif int % 5 == 0
+   # puts "Buzz"
+  #elsif int % 3 == 0 && int % 5 == 0
+    #puts "FizzBuzz"
+  #else
+    #puts 'nil'
   end
 end
